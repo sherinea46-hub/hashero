bf48a219af1d54c51970c35f405bf6145a3245e0
sherinea46-hub <sherinea46@gmail.com>
2025-09-12 17:08:06 -0400

    Add files via upload

--- assets/nav.js
+++ assets/nav.js
@@ -1,9 +1,14 @@
 
 (function(){
+  var isTools = /(^|\/)tools\//.test(location.pathname);
+  var P = isTools ? '../' : '';
   var NAV = ''
     + '<nav class="nav">'
     + '  <div class="nav-inner">'
-    + '    <a class="brand" href="index.html"><span>HashHero</span></a>'
+    + '    <a class="brand" href="'+P+'index.html">'
+    + '      <img src="'+P+'assets/logo.svg" alt="HashHero logo" style="width:22px;height:22px;vertical-align:middle" />'
+    + '      <span style="margin-left:8px">HashHero</span>'
+    + '    </a>'
     + '    <span class="brand-badge">Hashtags First</span>'
     + '    <div class="filler small">Add‑on tools for creators</div>'
     + '  </div>'
