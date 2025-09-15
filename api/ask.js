c71684cfddb4739be19ec7eef61039126408ad6f
sherinea46-hub <sherinea46@gmail.com>
2025-09-14 20:49:48 -0400

    Delete api/ask.js

--- api/ask.js
+++
@@ -1,8 +0,0 @@
-// api/ask.js - test version
-module.exports = async function handler(req, res) {
-  try {
-    return res.status(200).json({ ok: true, method: req.method });
-  } catch (err) {
-    return res.status(500).json({ error: String(err) });
-  }
-};
